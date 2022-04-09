703c4364b899d0bd4be188217a493e49641cdfee
razqyaqil <razqyaqil2@gmail.com>
2022-04-09 16:46:52 +0700

    “aplikasi bank transfer dengan runtime configuration

---
+++ modul7_1302202072/BankTransferConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace modul7_1302202072
+{
+    internal class BankTransferConfig
+    {
+        public string filePath = "bank_transfer_config.json";
+        public dynamic lang;
+        public int transferThreshold;
+        public int lowfee;
+        public int highfee;
+        public dynamic metod;
+        public dynamic confirm;
+
+        public BankTransferConfig()
+        {
+            StreamReader r = new StreamReader(filePath);
+            string jsonString = r.ReadToEnd();
+            JObject parsed = JObject.Parse(jsonString);
+
+
+            this.lang = parsed["lang"];
+            dynamic transfer = parsed["transfer"];
+
+            this.transferThreshold = transfer["threshold"];
+            this.lowfee = transfer["low_fee"];
+            this.highfee = transfer["high_fee"];
+            this.metod = parsed["methods"].ToArray();
+            this.confirm = parsed["confirmation"];
+
+
+        }
+
+
+    }
+}
--- modul7_1302202072/Program.cs
+++ modul7_1302202072/Program.cs
@@ -1,2 +1,113 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace modul7_1302202072 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            BankTransferConfig config = new BankTransferConfig();
+
+            int transfer = 0;
+            string lang = config.lang;
+            switch (lang)
+            {
+                case "en":
+                    Console.Write("Please insert the amount of money to transfer : ");
+                    transfer = Convert.ToInt32(Console.ReadLine());
+
+                    int fee = 0;
+                    if (transfer > config.transferThreshold)
+                    {
+                        fee = config.highfee;
+
+
+                    }
+                    else if (transfer <= config.transferThreshold)
+                    {
+                        fee = config.lowfee;
+                    }
+
+                    int biayaTf = transfer + fee;
+                    Console.WriteLine("Total amount: {0}", biayaTf);
+
+                    Console.WriteLine("Select Transfer Method");
+                    int i = 1;
+                    foreach (string metod in config.metod)
+                    {
+
+                        Console.WriteLine("{0} {1}", i, metod);
+                        i++;
+                    }
+                    string methodPilihan = Console.ReadLine();
+
+                    Console.WriteLine("Please type {0}", config.confirm[lang]);
+                    string confirm = Console.ReadLine();
+
+                    if (confirm == config.confirm[lang].ToString())
+                    {
+                        Console.WriteLine("The transfer is complete");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Transfer is Canceled");
+                    }
+
+                    break;
+
+                case "id":
+                    Console.Write("Masukan jumlah uang yang ingin di transfer : ");
+                    transfer = Convert.ToInt32(Console.ReadLine());
+                    fee = 0;
+                    if (transfer > config.transferThreshold)
+                    {
+                        fee = config.highfee;
+
+
+                    }
+                    else if (transfer <= config.transferThreshold)
+                    {
+                        fee = config.lowfee;
+                    }
+
+                    biayaTf = transfer + fee;
+                    Console.WriteLine("Total biaya: {0}", biayaTf);
+
+                    Console.WriteLine("Select Transfer Method");
+                    i = 1;
+                    foreach (string metod in config.metod)
+                    {
+
+                        Console.WriteLine("{0} {1}", i, metod);
+                        i++;
+                    }
+                    methodPilihan = Console.ReadLine();
+
+                    Console.WriteLine("Ketik {0}", config.confirm[lang]);
+                    confirm = Console.ReadLine();
+
+                    if (confirm == config.confirm[lang].ToString())
+                    {
+                        Console.WriteLine("Proses transfer berhasil");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Transfer dibatalkan");
+                    }
+
+                    break;
+                    
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
